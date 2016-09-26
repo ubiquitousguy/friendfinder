c993dc0beb3576c49cd7dae49a8bd928cd859ffb
Guy <guypham@hotmail.com>
2016-09-26 13:46:51 -0700

    updated files

---
+++ app/routing/api-routes.js
@@ -0,0 +1,19 @@
+var matchArray = require('../data/friends.js');
+var path = require('path');
+
+module.exports = function(app){
+
+    // GET Requests
+    // Shows json data on the page
+
+    app.get('/api/friends', function(req,res){
+        res.json(matchArray);
+    });
+
+    // POST Requests
+    // Handles data after submit form
+
+    app.post('/api/friends', function(req,res){
+        friendsData.push(req.body);
+    });
+}
