9ff945264bb7dd1c516c7a5839354a828dc07feb
Guy <guypham@hotmail.com>
2016-09-26 12:53:48 -0700

    added arrays and home and survey page

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+//create array
+var matchArray = [
+  {
+    "name": "Justin Bieber",
+    "photo": "https://en.wikipedia.org/wiki/File:Justin_Bieber_in_2015.jpg",
+    "answers": [
+      "1",
+      "3",
+      "3",
+      "4",
+      "2",
+      "1",
+      "5",
+      "1",
+      "5",
+      "1"
+    ]
+  },
+  {
+      "name": "Adele",
+      "photo": "https://en.wikipedia.org/wiki/File:Adele_2016.jpg"
+      "answers": [
+        "1",
+        "3",
+        "4",
+        "5",
+        "2",
+        "3",
+        "4",
+        "2",
+        "1",
+        "4"
+      ]
+  }
+];
+
+module.exports = matchArray;
